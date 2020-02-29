78b195d7921696abb8721360a95b80c6b641564b
root <root@DESKTOP-QRLQ7Q8.localdomain>
2020-02-29 09:46:13 +0100

    prettier

--- components/calendar.jsx
+++ components/calendar.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from 'react'
-import dayjs from 'dayjs'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import dayjs from "dayjs";
+import styled from "styled-components";
 
+const getMonthDays = (year, month) => new Date(year, month, 0).getDate();
 
-const getMonthDays = (year, month) => new Date(year, month, 0).getDate()
+const getDate = (year, month, day) => new Date(year, month, day);
 
-const getDate = (year, month, day) => new Date(year, month, day)
+const formatDate = date => dayjs(date).format("DD");
 
-const formatDate = date => dayjs(date).format('DD')
-
-const formatMonth = month => dayjs(getDate(2020, month, 1)).format("MMMM")
+const formatMonth = month => dayjs(getDate(2020, month, 1)).format("MMMM");
 
 const getMonthDaysList = (year, month) => {
-  const monthDays = getMonthDays(year, month)
-  return Array.from({ length: monthDays }, (_, day) => getDate(year, month - 1, day + 1))
-}
+	const monthDays = getMonthDays(year, month);
+	return Array.from({ length: monthDays }, (_, day) =>
+		getDate(year, month - 1, day + 1)
+	);
+};
 
 const Title = styled.div`
-margin-bottom: 1rem;
-margin: auto;
-
-`
+	margin-bottom: 1rem;
+	margin: auto;
+`;
 
 const Pointer = styled.div`
 width: 10px;
@@ -31,30 +31,39 @@ border-bottom: transparent 5px solid;
 border-top: transparent 5px solid;
 box-sizing: border-box;
 }
-`
+`;
 
 const CalendarHeader = styled.div`
-display:grid;
-grid-template-columns: 1fr 1fr 1fr;
-width: 600px;
-`
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	width: 600px;
+`;
 
 const CalendarGrid = styled.div`
-display: grid;
-grid-row-gap: 10px;
-width: 600px;
-grid-template-columns: repeat(7, 1fr);
-`
+	display: grid;
+	grid-row-gap: 10px;
+	width: 600px;
+	grid-template-columns: repeat(7, 1fr);
+`;
 
 export const Calendar = ({ year, holidays }) => {
-  const [month, setMonth] = useState(2)
-  const handleChangeMonthClick = steps => setMonth(prevMonth => prevMonth + steps)
-  const monthDaysList = getMonthDaysList(year, month)
+	const [month, setMonth] = useState(2);
+	const handleChangeMonthClick = steps =>
+		setMonth(prevMonth => prevMonth + steps);
+	const monthDaysList = getMonthDaysList(year, month);
 
-  return (
-    <>
-      <CalendarHeader><Pointer left onClick={() => handleChangeMonthClick(-1)} /><Title>{formatMonth(month - 1)}</Title><Pointer right onClick={() => handleChangeMonthClick(1)} /></CalendarHeader>
-      <CalendarGrid>{monthDaysList.map(day => <div key={day}>{formatDate(day)}</div>)}</CalendarGrid>
-    </>
-  )
-}
+	return (
+		<>
+			<CalendarHeader>
+				<Pointer left onClick={() => handleChangeMonthClick(-1)} />
+				<Title>{formatMonth(month - 1)}</Title>
+				<Pointer right onClick={() => handleChangeMonthClick(1)} />
+			</CalendarHeader>
+			<CalendarGrid>
+				{monthDaysList.map(day => (
+					<div key={day}>{formatDate(day)}</div>
+				))}
+			</CalendarGrid>
+		</>
+	);
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,31 +1,30 @@
-import React from 'react'
-import fetch from 'isomorphic-unfetch'
-import { Calendar } from '../components/calendar'
-
+import React from "react";
+import fetch from "isomorphic-unfetch";
+import { Calendar } from "../components/calendar";
 
 /*** Nager.Date Public Holiday API (https://date.nager.at/Api) ***/
 
 // single country yearly holidays
 const getHolidays = async (year, country) => {
-  const res = await fetch(`/api/PublicHolidays/${year}/${country}`)
-  return res.json()
-}
+	const res = await fetch(`/api/PublicHolidays/${year}/${country}`);
+	return res.json();
+};
 // available countries
 const getCountries = async () => {
-  const res = await fetch('/api/AvailableCountries')
-  return res.json()
-}
+	const res = await fetch("/api/AvailableCountries");
+	return res.json();
+};
 
 const Index = () => {
-  const year = 2020
-  const country = 'ES'
-  const [holidays, setHolidays] = React.useState([])
+	const year = 2020;
+	const country = "ES";
+	const [holidays, setHolidays] = React.useState([]);
 
-  React.useEffect(() => {
-    getHolidays(year, country).then(res => setHolidays(res))
-  }, [])
+	React.useEffect(() => {
+		getHolidays(year, country).then(res => setHolidays(res));
+	}, []);
 
-  return <Calendar year={year} holidays={holidays} />
-}
+	return <Calendar year={year} holidays={holidays} />;
+};
 
-export default Index
+export default Index;
